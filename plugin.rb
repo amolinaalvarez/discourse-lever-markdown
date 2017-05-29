c7e7c71e70d190c7d836461b7c5c88c7ca0808eb
Alvaro Molina Alvarez <alvaro@exo.works>
2017-05-29 16:30:53 +0200

    updating plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,7 @@
-# name: Discourse - Lever - Markdown
+# name: discourse-lever-markdown
 # about:  Define the markdown of ExO Lever over Discourse
 # version: 0.0.1
 # authors: Álvaro Molina Álvarez
+# url: https://github.com/amolinaalvarez/discourse-lever-markdown.git
 
 enabled_site_setting :exo_lever_markdown_enabled
